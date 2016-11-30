a21ba208ce6bf61a80af1913e0446da95db59ae5
nhanht <nhanht@onetech.vn>
2016-11-30 18:06:11 +0700

    Fix folder and List Schema

--- src/server/models/List.js
+++ src/server/models/List.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema
 var listSchema = new Schema({
 	title: {
 		type: String,
-		maxlength = [255, 'The value of path `{PATH}` (`{VALUE}`) exceeds the maximum allowed length ({MAXLENGTH}).'],
+		maxlength: [255, 'The value of path `{PATH}` (`{VALUE}`) exceeds the maximum allowed length ({MAXLENGTH}).'],
 		required: true
 	},
 	order: Number,
