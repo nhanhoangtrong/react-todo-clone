e2272183ea380f0c0da2d292696ebb5034b2d35e
siege250294 <siege.250294@gmail.com>
2016-11-30 07:16:50 +0700

    Connect database persitently

--- src/server/connectdb/index.js
+++ src/server/connectdb/index.js
@@ -1,2 +1,16 @@
 var mongoose = require('mongoose')
+let options = {
+    server: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000
+        }
+    },
+    replset: {
+        socketOptions: {
+            keepAlive: 1,
+            connectTimeoutMS: 30000
+        }
+    }
+};
 module.exports = mongoose.connect.bind(mongoose, 'mongodb://localhost:27017/todo-clone')
