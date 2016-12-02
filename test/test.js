91b798f4cd632bfc1a7e1ed2852dd8cd4a700dc9
nhanht <nhanht@onetech.vn>
2016-12-02 12:37:11 +0700

    Test: Folder API Tests

--- test/test.js
+++ test/test.js
@@ -16,7 +16,7 @@ chai.use(chaiHttp)
 server.listen(8080)
 
 describe('User', () => {
-    before( (done) => {
+    before((done) => {
         User.remove({}, (err) => {
             done()
         })
@@ -27,7 +27,7 @@ describe('User', () => {
         it('it should GET user profile', (done) => {
             chai.request(server)
                 .get('/api/profile/admin')
-                .end( (err, res) => {
+                .end((err, res) => {
                     res.should.have.status(400)
                     done()
                 })
@@ -39,14 +39,14 @@ describe('User', () => {
         let user = {
             username: "user1",
             password: "user1",
-            email   : "user1@email.com",
+            email: "user1@email.com",
             is_admin: false
         }
         it('it should POST a new user', (done) => {
             chai.request(server)
                 .post('/api/user/create')
                 .send(user)
-                .end( (err, res) => {
+                .end((err, res) => {
                     res.should.have.status(200)
                     res.body.should.have.property('_id')
                     res.body._id.should.be.a('string')
@@ -60,7 +60,7 @@ describe('User', () => {
         it('it should GET an user profile', (done) => {
             chai.request(server)
                 .get('/api/profile/user1')
-                .end( (err, res) => {
+                .end((err, res) => {
                     res.should.have.status(200)
                     res.body.should.have.property('_id')
                     res.body._id.should.be.a('string')
@@ -73,27 +73,65 @@ describe('User', () => {
 
 describe('Folder and List', () => {
 
+    let _user = ""
+
     // Clean all folders and lists
-    before( (done) => {
-        List.remove({}, function(err) {
-            Folder.remove({}, function(err) {
-                done()
+    before((done) => {
+        let user = new User({
+            username: "ownnerFolder",
+            password: "ownerFolder",
+            email: "userOwnerFolder@email.com",
+            is_admin: false
+        })
+        user.save(function(err) {
+            _user = user._id
+            done()
+        })
+    })
+
+    // Clean test user
+    after((done) => {
+        // Remove all test folder and list
+        List.remove({_user:_user}, function(err) {
+            Folder.remove({_user: _user}, function(err) {
+                // Then remove test user
+                User.findByIdAndRemove(_user, (err, raw) => {
+                    done()
+                })
             })
+        })
+        
+    })
+
+    // Test POST an folder
+    describe('/POST folder', () => {
+        it('it should POST create a new folder', (done) => {
+            let folder = {
+                title: "This is a test folder",
+                order: 1,
+                _user: _user
+            }
+            chai.request(server)
+                .post('/api/folder')
+                .send(folder)
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    res.body.should.be.a('object')
+                    done()
+                })
         })
     })
 
     // Test GET all folder
     describe('/GET folder', () => {
-        it('it should GET a empty folder list', (done) => {
+        it('it should GET a folder list', (done) => {
             chai.request(server)
                 .get('/api/folder')
-                .end( (err, res) => {
+                .end((err, res) => {
                     res.should.have.status(200)
                     res.body.should.be.a('array')
                     done()
                 })
         })
     })
-
-    // Test POST an folder
 })
