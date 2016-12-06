0ab32295aaa06fe18dcf2ef8a81a7370b7890253
nhanht <nhanht@onetech.vn>
2016-12-06 16:07:29 +0700

    Add action types

---
+++ src/client/store/actions/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+	// Todo action types
+	TODO_GET: 0,
+	TODO_CREATE: 1,
+	TODO_EDIT: 2,
+	TODO_MARK: 3,
+	TODO_REMOVE: 4,
+
+	// List action types
+	LIST_GET: 100,
+	LIST_CREATE: 101,
+	LIST_EDIT: 102,
+	LIST_REMOVE: 103,
+
+	// Folder action types
+	FOLDER_GET: 200,
+	FOLDER_CREATE: 201,
+	FOLDER_EDIT: 202,
+	FOLDER_REMOVE: 203,
+
+	// User action types
+	USER_GET: 300,
+	USER_EDIT: 301,
+	USER_CHANGE_PASSWORD: 302
+}
